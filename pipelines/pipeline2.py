ee8e0bd70ccc355521efab82430af4f4c61434a8
laura <laura.montaner@mail.udp.cl>
2025-06-30 15:13:14 -0400

    Pipelines Entrega 3

---
+++ pipelines/pipeline2.py
@@ -0,0 +1,12 @@
+
+###
+
+#para ejecutar los scrips pig:
+
+#entrar a la consola de procesamiento:
+ #docker exec -it procesamiento bash
+
+#pig /scripts/incidentes_por_comuna.pig
+#pig /scripts/frecuencia_tipos.pig
+#pig /scripts/tendencias_temporales.pig
+###
